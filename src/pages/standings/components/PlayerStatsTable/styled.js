aa228c2438726071f50a612d53b48585b09c62a1
unknown <nicosetzes@gmail.com>
2022-08-30 16:26:27 -0300

    V2.5 I think I've corrected some bug with MUI changing classnames, now the /standings view should be ok.

--- src/pages/standings/components/PlayerStatsTable/styled.js
+++ src/pages/standings/components/PlayerStatsTable/styled.js
@@ -3,4 +3,10 @@ import Table from '@mui/material/Table'
 
 export const StyledTable = styled(Table)`
   background-color: #004a79;
+  .MuiTableHead-root {
+    background-color: #1a1a2c;
+  }
+  .MuiTableBody-root {
+    background-color: #0d3d3e;
+  }
 `

02f3bf87703bf0bfd877a5165543d91fb063d253
Nicosetzes <nicosetzes@gmail.com>
2022-09-13 17:24:42 -0300

    V2.9.2 Trying something with CORS policy II.

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
-export const api = 'https://apa-website-be.onrender.com/api'
-// export const api = 'https://apa-website-be.vercel.app/api'
+// export const api = 'https://apa-website-be.onrender.com/api' // Si uso render, no hay problema con la CORS policy
+export const api = 'https://apa-website-be.vercel.app/api' // Al usar vercel, existe un problema con la CORS policy
 // export const api = 'http://localhost:5000/api'
 // export const api = 'https://apa-website-be.herokuapp.com/api'
 

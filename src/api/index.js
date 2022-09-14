3554c4905399fe30e2ecb659627278d0cea50239
Nicosetzes <nicosetzes@gmail.com>
2022-09-14 01:12:55 -0300

    V3.0 I'm trying to connect with the BE deployed on Vercel.

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
-export const api = 'https://apa-website-be.onrender.com/api' // Si uso render, no hay problema con la CORS policy
-// export const api = 'https://apa-website-be.vercel.app/api' // Al usar vercel, existe un problema con la CORS policy
+// export const api = 'https://apa-website-be.onrender.com/api' // Si uso render, no hay problema con la CORS policy
+export const api = 'https://apa-website-be.vercel.app/api' // Al usar vercel, existe un problema con la CORS policy
 // export const api = 'https://apa-website-be.netlify.app/api'
 // export const api = 'http://localhost:5000/api'
 // export const api = 'https://apa-website-be.herokuapp.com/api'
 
-export const database = 'https://apa-database.onrender.com/api'
-// export const database = 'https://apa-football-database.vercel.app/api'
+// export const database = 'https://apa-database.onrender.com/api'
+export const database = 'https://apa-football-database.vercel.app/api'
 // export const database = 'http://localhost:8080/api'

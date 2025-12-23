fbd7a8600ddf174dd084a8596e9878d9e99d5019
Nicosetzes <nicosetzes@gmail.com>
2025-12-23 19:04:58 -0300

    Fixed consts in /api (new development branch)

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
-// export const api = 'https://apa-website-be.vercel.app/api'
-export const api = 'http://localhost:5000/api'
+export const api = 'https://apa-website-be.vercel.app/api'
+// export const api = 'http://localhost:5000/api'
 
-// export const database = 'https://apa-football-database.vercel.app/api'
-export const database = 'http://localhost:8080/api'
+export const database = 'https://apa-football-database.vercel.app/api'
+// export const database = 'http://localhost:8080/api'
 
 export const cloudName = 'apa-images-repository'
